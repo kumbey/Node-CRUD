6d708c522dbe4aaa4e9c2f51124d72483d3bbdf8
kumbey <74360101+kumbey@users.noreply.github.com>
2021-01-04 20:53:33 +0700

    final update
    update other functions

--- Backend/Routes/PostRoutes.js
+++ Backend/Routes/PostRoutes.js
@@ -7,6 +7,19 @@ const postController = require("../Controllers/PostController");
 // use
 router.get("/", postController.baseRoute);
 
+// create
+router.post("/create", postController.createPost);
+
+// read all
 router.get("/getPosts", postController.getPosts);
+
+// read one
+router.get("/getPost/:id", postController.getSinglePost);
+
+// update
+router.put("/post/:id/update", postController.updatePost);
+
+// delete
+router.delete("/delete/:id", postController.deletePost);
 
 module.exports = router;
